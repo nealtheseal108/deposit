6e91f53b48f08e36d60f5d6400ae3ce89aa24cfd
nealtheseal108 <68606508+nealtheseal108@users.noreply.github.com>
2023-03-20 23:26:47 -0700

    Update SecureWallet.sol

--- SecureWallet.sol
+++ SecureWallet.sol
@@ -14,19 +14,19 @@ contract Deposit {
     function receiveFunds() payable external {
         user.fundAllocation[msg.sender] = user.fundAllocation[msg.sender] + msg.value;
         if (user.prefUnit <= 1) {
-            console.log("Contract has received %s wei from your address; your current balance is %s wei.", msg.value, msg.sender, user.fundAllocation[msg.sender]);
+            console.log("Contract has received %s wei from your address; your current balance is %s wei.", msg.value, user.fundAllocation[msg.sender]);
         } else if (user.prefUnit == 3) {
-            console.log("Contract has received %s kwei from your address, or %s wei; your current balance is %s kwei.", msg.value / 1000, msg.sender, user.fundAllocation[msg.sender] / 1000);
+            console.log("Contract has received %s kwei from your address, or %s wei; your current balance is %s kwei.", msg.value / 1000,  user.fundAllocation[msg.sender] / 1000);
         } else if (user.prefUnit == 6) {
-            console.log("Contract has received %s mwei from your address, or %s wei; your current balance is %s mwei.", msg.value / 1000000, msg.sender, user.fundAllocation[msg.sender] / 1000000);
+            console.log("Contract has received %s mwei from your address, or %s wei; your current balance is %s mwei.", msg.value / 1000000, user.fundAllocation[msg.sender] / 1000000);
         } else if (user.prefUnit == 9) {
-            console.log("Contract has received %s gwei from your address, or %s wei; your current balance is %s gwei.", msg.value / 1000000000, msg.sender, user.fundAllocation[msg.sender] / 10000000000);
+            console.log("Contract has received %s gwei from your address, or %s wei; your current balance is %s gwei.", msg.value / 1000000000, user.fundAllocation[msg.sender] / 10000000000);
         } else if (user.prefUnit == 12) {
-            console.log("Contract has received %s szabo, or microether, from your address, or %s wei; your current balance is %s szabo.", msg.value / 1000000000000, msg.sender, user.fundAllocation[msg.sender] / 10000000000000);
+            console.log("Contract has received %s szabo, or microether, from your address, or %s wei; your current balance is %s szabo.", msg.value / 1000000000000, user.fundAllocation[msg.sender] / 10000000000000);
         } else if (user.prefUnit == 15) {
-            console.log("Contract has received %s finney, or milliether, from your address, or %s ; your current balance is %s finney.", msg.value / 1000000000000000, msg.sender, user.fundAllocation[msg.sender] / 10000000000000000);
+            console.log("Contract has received %s finney, or milliether, from your address, or %s ; your current balance is %s finney.", msg.value / 1000000000000000, user.fundAllocation[msg.sender] / 10000000000000000);
         } else if (user.prefUnit == 18) {
-            console.log("Contract has received %s ether from your address, or %s wei; your current balance is %s ether.", msg.value / 1000000000000000000, msg.sender, user.fundAllocation[msg.sender] / 10000000000000000000);
+            console.log("Contract has received %s ether from your address, or %s wei; your current balance is %s ether.", msg.value / 1000000000000000000, user.fundAllocation[msg.sender] / 10000000000000000000);
         }
     }
 
@@ -57,7 +57,7 @@ contract Deposit {
 
     function getBalanceSender() view public returns (uint) {
         if (user.prefUnit <= 1) {
-            console.log("You, or %s, have a balance of %s wei, or %s ether.", msg.sender, user.fundAllocation[msg.sender], (user.fundAllocation[msg.sender]));
+            console.log("You, or %s, have a balance of %s wei.", msg.sender, user.fundAllocation[msg.sender], (user.fundAllocation[msg.sender]));
         } else if (user.prefUnit == 3) {
             console.log("You, or %s, have a balance of %s wei, or %s kwei.", msg.sender, user.fundAllocation[msg.sender], (user.fundAllocation[msg.sender] / 1000));
         } else if (user.prefUnit == 6) {
